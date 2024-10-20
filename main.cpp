ee5f52f45f28f8746c0e0aa11c943d8faf96060c
Sebastian Mira Pacheco <sebastianandresmira@gmail.com>
2024-10-20 12:45:21 -0300

    Instalación MonaEngine en el directorio

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "MonaEngine.hpp"
+#include "player.h"
+class MinimalSetup : public Mona::Application
+{
+public:
+	MinimalSetup() = default;
+	~MinimalSetup() = default;
+	virtual void UserStartUp(Mona::World& world) noexcept override {
+	}
+
+	virtual void UserShutDown(Mona::World& world) noexcept override {
+	}
+	virtual void UserUpdate(Mona::World& world, float timeStep) noexcept override {
+	}
+};
+int main()
+{
+	MinimalSetup app;
+	Mona::Engine engine(app);
+	engine.StartMainLoop();
+
+}
